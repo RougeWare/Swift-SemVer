9f30a6c4b9c264266c07c1658e7e3896a9346aa6
Ben Leggiero <BenLeggiero@Gmail.com>
2020-04-24 18:29:52 -0600

    Oh hey it works fine on tvOS! Might as well support that.

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     platforms: [
         .macOS(.v10_10),
         .iOS(.v11),
+        .tvOS(.v11),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
